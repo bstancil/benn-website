c7c991b493da1e36189ead1821d4fe72f455393a
Benn Stancil <bennstancil@gmail.com>
2025-07-11 16:30:09 -0400

    related posts + read button

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,8 @@ from app import opengraph
 
 from flask import Flask, render_template
 
+import requests
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -15,15 +17,24 @@ def twitter_interstitial():
 def dynamic_page(dynamic_page):
     
     substack_url = f'https://benn.substack.com/p/{dynamic_page}'
+    related_url = f'https://benn.substack.com/api/v1/archive?sort=new&search=&offset=0&limit=11'
     
-    ogs = opengraph.OpenGraph(url=substack_url) 
+    ogs = opengraph.OpenGraph(url=substack_url)
     
     if ogs.is_valid() and 'benn.substack' not in ogs.title:
+
+        try:
+            js = requests.get(related_url).json()
+            related = [{"title":r['title'], "slug":r['slug']} for r in js if r['slug'] != dynamic_page]
+        except:
+            related = []
+
         return render_template('post.html', 
             title=ogs.title, 
             image=ogs.image, 
             description=ogs.description, 
-            substack_url=substack_url
+            substack_url=substack_url,
+            related=related
         )
         
     else:
